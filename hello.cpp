a2588cd1b4dd2d0a6fd1ac23ed129c9721c918db
wyylucky <wyy_coding@126.com>
2014-11-27 23:32:45 +0800

    add new feature

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: parker 
+	> Mail: wyy_coding@126.com 
+	> Created Time: Thu 27 Nov 2014 11:27:14 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "hello github!" << endl;
+
+    return 0;
+}
